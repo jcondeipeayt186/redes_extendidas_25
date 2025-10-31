33bb2fb1c2be211adec5d07d5b331f1c2b3c705f
Julián <juliancond@gmail.com>
2025-10-31 12:31:19 -0300

    info de sql

--- introWebModeloOSIEnClase/db/redesiniciandoflask.sql
+++ introWebModeloOSIEnClase/db/redesiniciandoflask.sql
@@ -10,3 +10,9 @@ CREATE TABLE IF NOT EXISTS protocolos (
     fechaActualizacion DATE NOT NULL,
     descripcion TEXT
 ); 
+
+-- MySQL proporciona cuatro subtipos de TEXT:
+-- TINYTEXT: Almacena hasta 255 bytes. 
+-- TEXT: Almacena hasta 65,535 bytes (aproximadamente 64KB). 
+-- MEDIUMTEXT: Almacena hasta 16,777,215 bytes (aproximadamente 16MB). 
+-- LONGTEXT: Almacena hasta 4,294,967,295 bytes (aproximadamente 4GB). 
